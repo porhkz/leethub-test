d07bfe0f42893cb3a437a1a6c0fc07a110e38469
Will Sam <71242485+porhkz@users.noreply.github.com>
2026-07-28 09:16:08 +1000

    Add LeetCode solution: 125. Valid Palindrome

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -1,19 +1,21 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        if len(s) == 0 or len(s) == 1:
-            return True
-        
-        list_s = [c for c in s if c.isalpha() and c.islower()]
+    def isPalindrome(self, s: str) -> bool:        
+        list_s = [c for c in s if c.isalnum()]
+        list_s = [c.lower() for c in list_s]
         s = "".join(list_s)
         right = len(s)-1
-        
-        print(s)
+
+        if len(s) == 0 or len(s) == 1:
+            return True
 
         for i in range(len(s)):
+            if i == right:
+                return True
+            
             if s[i] != s[right]:
                 return False
             
-            right = len(s) - 1
+            right = right - 1
 
-        return False
+        return True
             

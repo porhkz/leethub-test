54b5b20f194b340a1eb556c23fc87f34a88ce240
Will Sam <71242485+porhkz@users.noreply.github.com>
2026-08-15 13:39:55 +1000

    Add LeetCode solution: 778. Reorganize String

--- 778. Reorganize String.py
+++ 778. Reorganize String.py
@@ -5,7 +5,7 @@ class Solution:
     def reorganizeString(self, s: str) -> str:
         freq = Counter(s)
         freq_list = []
-        result = ""
+        result = []
 
         for key, value in freq.items():
             freq_list.append((value, key))
@@ -13,21 +13,19 @@ class Solution:
         heapq.heapify_max(freq_list)
 
         for i in range(len(s)):
-            print(freq_list)
 
             count, char = heapq.heappop_max(freq_list)
 
             if len(result) != 0 and char == result[len(result) - 1]:
-                if len(freq_list) == 1:
-                    result = ""
+                if len(freq_list) == 0:
+                    result = []
                     break
                 tmp_count, tmp_char, = heapq.heappop_max(freq_list)
                 heapq.heappush_max(freq_list, (count, char))
                 count = tmp_count
                 char = tmp_char
 
-            result += char
-            print(result)
+            result.append(char)
 
             count -= 1
 
@@ -36,7 +34,8 @@ class Solution:
             else:
                 heapq.heappush_max(freq_list, (count, char))
 
-        return result
+        result_s = "".join(result)
+        return result_s
 
 
 
